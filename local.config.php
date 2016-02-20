21aec2d4992688c72604fc1b1d66ea3b3ea8366e
samo@stellarbluetechnologies.com <samo@stellarbluetechnologies.com>
2016-02-20 10:48:57 -0600

    first commit

---
+++ local.config.php
@@ -0,0 +1,6 @@
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
+define('DB_HOST', 'localhost');
