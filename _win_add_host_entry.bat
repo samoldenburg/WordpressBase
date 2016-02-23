d8203337263e76a536f26ed65660e447976f0bbc
samo@stellarbluetechnologies.com <samo@stellarbluetechnologies.com>
2016-02-22 18:36:53 -0600

    windows batch file for adding hosts and vhosts entries

---
+++ _win_add_host_entry.bat
@@ -0,0 +1,101 @@
+@echo off
+TITLE Modifying your HOSTS file
+COLOR F0
+ECHO.
+
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+:LOOP
+SET Choice=
+SET /P Choice="Do you want to modify HOSTS file ? (Y/N)"
+
+IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+
+ECHO.
+IF /I '%Choice%'=='Y' GOTO ACCEPTED
+IF /I '%Choice%'=='N' GOTO REJECTED
+ECHO Please type Y (for Yes) or N (for No) to proceed!
+ECHO.
+GOTO Loop
+
+
+:REJECTED
+ECHO Your HOSTS file was left unchanged>>%systemroot%\Temp\hostFileUpdate.log
+ECHO Finished.
+GOTO END
+
+
+:ACCEPTED
+
+echo Enter the name you'd like to access this development script at.
+set /p hostname="Enter Your Hostname: "
+
+setlocal enabledelayedexpansion
+::Create your list of host domains
+set LIST=(%hostname%)
+::Set the ip of the domains you set in the list above
+set %hostname%=127.0.0.1
+:: deletes the parentheses from LIST
+set _list=%LIST:~1,-1%
+::ECHO %WINDIR%\System32\drivers\etc\hosts > tmp.txt
+for  %%G in (%_list%) do (
+    set  _name=%%G
+    set  _value=!%%G!
+    SET NEWLINE=^& echo.
+    ECHO Carrying out requested modifications to your HOSTS file
+    ::strip out this specific line and store in tmp file
+    type %WINDIR%\System32\drivers\etc\hosts | findstr /v !_name! > tmp.txt
+    ::re-add the line to it
+    ECHO %NEWLINE%^!_value! !_name!>>tmp.txt
+    ::overwrite host file
+    copy /b/v/y tmp.txt %WINDIR%\System32\drivers\etc\hosts
+    del tmp.txt
+)
+ipconfig /flushdns
+ECHO Added hosts line
+GOTO END
+
+:END
+echo Adding virtual host
+@echo off
+(
+echo:
+echo:
+echo   ### %hostname% ###
+echo   ^<VirtualHost %hostname%:80^>
+echo       DocumentRoot "%~dp0web"
+echo       ServerName %hostname%
+echo       ^<Directory "%~dp0web"^>
+echo         AllowOverride All
+echo         Order allow,deny
+echo         allow from all
+echo       ^</Directory^>
+echo   ^</VirtualHost^>
+) >>C:\xampp\apache\conf\extra\httpd-vhosts.conf
+echo You'll have to restart apache now. Happy coding!
+pause
+EXIT
